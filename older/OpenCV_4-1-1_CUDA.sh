6e51feb598ff19aea87ffa3f26f471e1172c8f76
Muhammad Luqman Bukhori <44484883+mluqmanbukhori@users.noreply.github.com>
2022-03-19 11:03:42 +0700

    Update OpenCV_4-1-1_CUDA.sh

--- older/OpenCV_4-1-1_CUDA.sh
+++ older/OpenCV_4-1-1_CUDA.sh
@@ -13,10 +13,9 @@ sudo apt-get install -y build-essential cmake git unzip pkg-config
 sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev
 sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install -y libgtk2.0-dev libcanberra-gtk*
-sudo apt-get install -y python3-dev python3-numpy python3-pip
 sudo apt-get install -y libxvidcore-dev libx264-dev libgtk-3-dev
 sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev
-sudo apt-get install -y gstreamer1.0-tools libv4l-dev v4l-utils
+sudo apt-get install -y libv4l-dev v4l-utils
 sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 sudo apt-get install -y libavresample-dev libvorbis-dev libxine2-dev
 sudo apt-get install -y libfaac-dev libmp3lame-dev libtheora-dev
@@ -25,6 +24,7 @@ sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
 sudo apt-get install -y liblapack-dev libeigen3-dev gfortran
 sudo apt-get install -y libhdf5-dev protobuf-compiler
 sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
+sudo apt-get install -y qt5-default
 
 # remove old versions or previous builds
 cd ~ 
@@ -62,8 +62,9 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D CUDA_FAST_MATH=ON \
 -D OPENCV_DNN_CUDA=ON \
 -D ENABLE_NEON=ON \
--D WITH_QT=ON \
+-D WITH_QT=OFF \
 -D WITH_OPENMP=ON \
+-D WITH_OPENGL=ON \
 -D BUILD_TIFF=ON \
 -D WITH_FFMPEG=ON \
 -D WITH_GSTREAMER=ON \
@@ -76,7 +77,6 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D OPENCV_ENABLE_NONFREE=ON \
 -D INSTALL_C_EXAMPLES=OFF \
 -D INSTALL_PYTHON_EXAMPLES=OFF \
--D BUILD_NEW_PYTHON_SUPPORT=ON \
 -D BUILD_opencv_python3=TRUE \
 -D OPENCV_GENERATE_PKGCONFIG=ON \
 -D BUILD_EXAMPLES=OFF ..
